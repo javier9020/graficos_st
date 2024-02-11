b6dbe8f572a68675a9f5ab22924b99c7b7c1ba8e
javier9020 <66536233+javier9020@users.noreply.github.com>
2024-02-11 08:00:47 -0300

    Update Prueba_doc.py

--- Prueba_doc.py
+++ Prueba_doc.py
@@ -26,9 +26,6 @@ st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
 # Ejemplo 5
 
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['Eje x', 'Eje Y', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+df2 = pd.DataFrame(np.random.randn(200, 3),columns=['Eje x', 'Eje Y', 'c'])
+c = alt.Chart(df2).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
