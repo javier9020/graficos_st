56e19bacc9e66c562a1f6027727711fc301aa696
javier9020 <66536233+javier9020@users.noreply.github.com>
2024-02-11 08:30:25 -0300

    Update Prueba_doc.py

--- Prueba_doc.py
+++ Prueba_doc.py
@@ -8,7 +8,7 @@ st.header('st.write')
 
 # Ejemplo 1
 
-st.write('Hola, *Test Streamlit!* :sunglasses:')
+st.write('Hello, *World!* :sunglasses:')
 
 # Ejemplo 2
 
@@ -26,6 +26,8 @@ st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
 
 # Ejemplo 5
 
-df2 = pd.DataFrame(np.random.randn(200, 3),columns=['a', 'b', 'c'])
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['x', 'b', 'c'])
 c = alt.Chart(df2).mark_circle().encode(alt.X('x').title('Eje x'), y='b', size='c', color='c', tooltip=['x', 'b', 'c'])
 st.write(c)
